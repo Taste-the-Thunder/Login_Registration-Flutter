41dac6c45b7077a51c1bba5485dd51bad9f572aa
Dhruvin <35141691+dhruvinprajapati@users.noreply.github.com>
2022-08-28 15:45:57 +0530

    make Signup Page, update readme file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Login Register App
 
--  Simple Login Registration App With TextField Validation
+-  Simple Login Registration App With TextFormField Validation
+
+<img src="assets/login.png" width="300" height="600">
+<img src="assets/signup.png" width="300" height="600">
--- lib/signUpPage.dart
+++ lib/signUpPage.dart
@@ -9,11 +9,150 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('Sign Up'),
+        body: Form(
+          key: formKey,
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue[100],
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: [
+                      Text('Sign Up',
+                        style: TextStyle(
+                            fontSize: 35),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextFormField(
+                        validator: (value){
+                          if(value!.length <= 3){
+                            return 'please enter some value';
+                          } if(value!.isEmpty){
+                            return 'please Enter valide name';
+                          } else {
+                            return null;
+                          }
+                        },
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            hintText: 'Name',
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            )
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextFormField(
+                        validator: (value){
+                          if(value!.length != 10){
+                            return 'Number is not currect';
+                          } else {
+                            return null;
+                          }
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            hintText: 'Number',
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            )
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextFormField(
+                        validator: (value){
+                          if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)){
+                            return 'Please Enter Valide Address';
+                          } else {
+                            return null;
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            hintText: 'Email',
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            )
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextFormField(
+                        validator: (value){
+                          if(value!.length <= 6 || value!.length >= 16){
+                            return 'Password between 6 to 16 charecter';
+                          } else {
+                            return null;
+                          }
+                        },
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            hintText: 'Password',
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            )
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      ElevatedButton(
+                          onPressed: (){
+                            if(formKey.currentState!.validate()){
+                              print('hurrreee');
+                            } else {
+                              print('something is wrong');
+                            }
+                          },
+                          child: Text('Submit')
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      InkWell(
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const SignUpPage()),
+                          );
+                        },
+                        child: Text('Sign Up',
+                          style: TextStyle(
+                              fontSize: 35),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
